27cc485367335ecfbbf52802fc828a5e4c154226
3arpbl3y <3arpbl3y@gmail.com>
2022-04-04 06:06:25 +0500

    basic layout

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let a = "";
+let b = "";
+let sign = "";
+let finish = false;
+
+const digit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const action = ["-", "+", "*", "/"];
+
+//screen
+
+const out = document.querySelector(".calc-screen p");
+
+function clearAll() {
+  a = "";
+  b = "";
+  sign = "";
+  finish = false;
+  out.textContent = 0;
+}
+
+document.querySelector("reset").onclick = clearAll;
