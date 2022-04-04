bae98e2cb8c908da1838f28191ff34aa861d0036
3arpbl3y <3arpbl3y@gmail.com>
2022-04-04 17:56:09 +0500

    clearAll function for reset, digits and actions in console

--- js/script.js
+++ js/script.js
@@ -20,4 +20,42 @@ function clearAll() {
   out.textContent = 0;
 }
 
-document.querySelector("reset").onclick = clearAll;
+document.querySelector(".reset").onclick = clearAll;
+
+const keytest = document.querySelector(".keytest");
+
+// document.addEventListener("keypress", (e) => {
+//   console.log(e);
+//   if (e.keyCode === 32) {
+//     clearAll();
+//   }
+// });
+
+document.addEventListener("keypress", (e) => {
+  if (e.key === " ") {
+    clearAll();
+  }
+});
+
+document.querySelector(".buttons").onclick = (event) => {
+  if (!event.target.classList.contains("btn")) return;
+  if (event.target.classList.contains("reset")) return;
+  out.textContent = "";
+  const key = event.target.textContent;
+
+  if (digit.includes(key)) {
+    if (b === "" && sign === "") {
+      a += key;
+
+      console.log(a, b, sign);
+      out.textContent = a;
+    } else if (a != '' && b!== '' && finish)
+  }
+
+  if (action.includes(key)) {
+    sign = key;
+    out.textContent = sign;
+    console.log(a, b, sign);
+    return;
+  }
+};
