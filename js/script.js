aed7da1f284a00618c2790ce51717b966adee453
3arpbl3y <3arpbl3y@gmail.com>
2022-04-05 21:35:20 +0500

    dot sign working now

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ let b = "";
 let sign = "";
 let finish = false;
 
-const digit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const digit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
 const action = ["-", "+", "*", "/"];
 
 //screen
